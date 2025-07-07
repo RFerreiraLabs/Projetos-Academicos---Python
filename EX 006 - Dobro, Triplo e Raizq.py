88c6ea73783096292a76eb008baa5eb02c43fe6d
RFerreiraLabs <rodrigo_santos2806@hotmail.com>
2025-07-07 15:46:56 -0300

    Rename EX 006.py to EX 006 - Dobro, Triplo e Raizq.py

---
+++ EX 006 - Dobro, Triplo e Raizq.py
@@ -0,0 +1,6 @@
+n = int(input('digite um numero: '))
+dobro = n * 2
+triplo = n * 3
+raizn = pow(n, (1/2))
+
+print ('O dobro de {} é {}, o triplo é {} e a sua raiz quadrada é {:.2f}'.format(n, dobro, triplo, raizn))
